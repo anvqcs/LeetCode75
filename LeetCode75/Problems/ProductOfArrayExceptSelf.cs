65a616a38016f2724fad7c9cca239428aed001a8
anvqcs <anvqcs@gmail.com>
2024-03-24 11:28:42 +0700

    238. Product of Array Except Self

---
+++ LeetCode75/Problems/ProductOfArrayExceptSelf.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode75.Problems
+{
+    internal class ProductOfArrayExceptSelf
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            int[] res = new int[nums.Length];
+
+            res[0] = 1;
+            for (int i = 1; i< nums.Length; i++)
+            {
+                res[i] = nums[i - 1] * res[i - 1];
+            }
+
+            int rightProduct = 1;
+            for (int i = nums.Length - 1; i >=0; i--)
+            {
+                res[i] *= rightProduct;
+                rightProduct *= nums[i];
+            }
+            return res;
+        }
+    }
+}
--- LeetCode75/Program.cs
+++ LeetCode75/Program.cs
@@ -1,5 +1,1 @@
 ﻿using LeetCode75.Problems;
-
-if (string.IsNullOrEmpty("          ".Trim()))
-    Console.WriteLine("Null");
-Console.WriteLine(new ReverseWordsInAString().ReverseWords("ab        bc"));
