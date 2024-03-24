c3c9f30b79e1a7a9b5c562517ac8562c75d9fe46
anvqcs <anvqcs@gmail.com>
2024-03-24 10:24:45 +0700

    1768. Merge Strings Alternately

---
+++ LeetCode75/Problems/MergeAlternately.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode75.Problems
+{
+    public class MergeAlternately
+    {
+        public string MergeStringsAlternately(string word1, string word2)
+        {
+            if (string.IsNullOrEmpty(word1)) return word2;
+            if (string.IsNullOrEmpty(word2)) return word1;
+
+            string res = "";
+            for (int i = 0; i < Math.Min(word1.Length, word2.Length); i++)
+            {
+                res += word1[i].ToString() + word2[i].ToString();
+            }
+            if (word1.Length > word2.Length) return res + word1.Substring(word2.Length, word1.Length - word2.Length);
+            return res + word2.Substring(word1.Length, word2.Length - word1.Length);
+        }
+    }
+}
--- LeetCode75/Program.cs
+++ LeetCode75/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LeetCode75.Problems;
+
+Console.WriteLine(new MergeAlternately().MergeStringsAlternately("ab", "pqrs"));
